86bf437d1485213c4b5d00354f44e66ec6c5e636
mushrambom <felimpe.a.mendes@gmail.com>
2020-09-11 10:01:59 -0300

    added render new note and new project functionalities

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const newNoteForm = document.getElementById('new-note-form');
+const newProjectForm = document.getElementById('new-project-form');
 const createProject = function(title, description) {
     const noteArray = [];
     const addNote = function(title, content) {
@@ -8,7 +9,7 @@ const createProject = function(title, description) {
     }
     return {
         title,
-        content,
+        description,
         addNote,
     }
 };
@@ -21,7 +22,7 @@ const createNote = function(title, content) {
 const userProjects = (function() {
     const projectArray = [];
     const addProject = function(title, description) {
-        const newProject = createProject(title, content);
+        const newProject = createProject(title, description);
         projectArray.push(newProject);
         return newProject;
     }
@@ -31,12 +32,35 @@ const userProjects = (function() {
         defaultProject,
     }
 })();
+const renderNote = function(note) {
+    const content = document.getElementById('content-card');
+    const noteHeader = document.createElement('h2');
+    const noteContent = document.createElement('p');
+    noteHeader.innerText = note.title;
+    noteContent.innerText = note.content;
+    content.append(noteHeader, noteContent);
+};
+const renderProject = function(project) {
+    const projectsList = document.getElementById('projects-list');
+    const projectLi = document.createElement('li');
+    projectLi.innerText = project.title;
+    projectLi.title = project.description;
+    projectsList.append(projectLi);
+};
 newNoteForm.addEventListener('submit', function(e) {
     e.preventDefault();
     const noteTitle = document.getElementById('title').value;
     const noteContent = document.getElementById('content').value;
     const defaultProject = userProjects.defaultProject;
     const newNote = defaultProject.addNote(noteTitle, noteContent);
-    console.log(defaultProject);
-    console.log(newNote);
+    renderNote(newNote);
 });
+newProjectForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const projectTitle = document.getElementById('project-title').value;
+    const projectDescription = document.getElementById('project-description').value;
+    const newProject = userProjects.addProject(projectTitle, projectDescription);
+    renderProject(newProject);
+});
+
+
